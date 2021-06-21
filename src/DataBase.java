b2dc4961d833b3c739db0654bfabf5182b0144ef
YehudaSy <51841689+YehudaSy@users.noreply.github.com>
2021-06-21 20:00:40 +0300

    2106 2000

--- src/DataBase.java
+++ src/DataBase.java
@@ -1,3 +1,6 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -14,14 +17,21 @@ public class DataBase {
     }
 
     public void findHyponymSuchAs() {
-        String rgx =
-                "(<np>[^<]*</np>\\s+)(such\\s+as\\s+)(<np>[^<]*</np>)"
-                        + "((\\s*(,)?\\s*)(\\s*or\\s*|\\s*and\\s*)?(<np>[^<]*</np>))*";
+        String rgx = "(<np>[^<]*</np>\\s+)(such\\s+as\\s+)(<np>[^<]*</np>)"
+                + "((\\s*(,)?\\s*)(\\s*or\\s*|\\s*and\\s*)?(<np>[^<]*</np>))*";
         Pattern p = Pattern.compile(rgx);
-        int i = 0;
+        PrintWriter writer = null;
+        try {
+            writer =
+                    new PrintWriter("C:\\Users\\USER\\IdeaProjects\\ass7\\hypernym_db.txt", StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         for (String line : rawData.getLines()) {
             Matcher m = p.matcher(line);
             while (m.find()) {
+                assert writer != null;
+                writer.println(m.group());
                 System.out.println(m.group());
             }
         }
